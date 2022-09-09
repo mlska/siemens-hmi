590419e0bec2638cbe51c4ddd0f12c1477ae3d0a
Kamil <kamil.malesa1@gmail.com>
2022-09-09 22:38:10 +0200

    added constants and time stamps

--- server/constants.js
+++ server/constants.js
@@ -1,11 +1,10 @@
 "use strict";
-
-const variables = {
-  wStatusWord1: "DB1,WORD0",
-  wControlWord1: "DB1,WORD2",
-  iAccessLevel: "DB1,INT4",
-  iServoOverride: "DB1,INT6",
-  a_BStatusPneumatics: "DB1,BYTE8.11",
-};
-
-module.exports = variables;
+(exports.configuration = { host: "192.168.0.100", rack: 0, slot: 1 }),
+  (exports.refreshTime = 1000),
+  (exports.variables = {
+    wStatusWord1: "DB1,WORD0",
+    wControlWord1: "DB1,WORD2",
+    iAccessLevel: "DB1,INT4",
+    iServoOverride: "DB1,INT6",
+    a_BStatusPneumatics: "DB1,BYTE8.11",
+  });
--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 const { S7Endpoint, S7ItemGroup } = require("nodes7");
-const variables = require("./constants");
+const { configuration, variables, refreshTime } = require("./constants");
 
 const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
+
+const timeStamp = () => new Date().toISOString().slice(0, 19);
 
 const io = new Server(server, {
   cors: {
@@ -20,30 +22,30 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   let interval;
 
-  console.log(`User Connected: ${socket.id}`);
+  console.log(`${timeStamp()} - HMI Connected: ${socket.id}`);
 
-  const plc = new S7Endpoint({ host: "192.168.0.100", rack: 0, slot: 1 });
-  const itemGroup = new S7ItemGroup(plc);
+  const plc = new S7Endpoint(configuration);
+  const tags = new S7ItemGroup(plc);
 
-  plc.on("error", (e) => console.log("PLC Error!", e));
+  plc.on("error", (e) => console.log(`${timeStamp()} - PLC`, e));
   plc.on("disconnect", () => {
-    console.log("PLC Disconnected");
+    console.log(`${timeStamp()}  - PLC Disconnected`);
     clearInterval(interval);
   });
 
   plc.on("connect", async () => {
-    console.log("PLC Connected!");
+    console.log(`${timeStamp()} - PLC Connected`);
 
-    itemGroup.setTranslationCB((tag) => variables[tag]);
-    itemGroup.addItems(Object.keys(variables));
+    tags.setTranslationCB((tag) => variables[tag]);
+    tags.addItems(Object.keys(variables));
     interval = setInterval(async () => {
-      const data = await itemGroup.readAllItems();
-      socket.broadcast.emit("receive_message", data);
-    }, 2000);
+      const data = await tags.readAllItems();
+      socket.broadcast.emit("data", data);
+    }, refreshTime);
 
-    socket.on("message", (value) => {
-      console.log(value);
-      itemGroup.writeItems("wStatusWord1", value);
+    socket.on("control", (variable) => {
+      console.log(variable);
+      tags.writeItems(variable.name, variable.value);
     });
   });
 });
