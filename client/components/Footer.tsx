9b82758f2cb0fe8891c641586edad7eb8c4f147a
Kamil <kamil.malesa1@gmail.com>
2022-09-12 19:41:53 +0200

    added footer

---
+++ client/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="flex items-center justify-center w-full h-12 font-light text-white bg-blue-600">
+      Custom Human Machine Interface with Typescript & Next.js & Tailwind
+    </footer>
+  );
+};
+
+export default Footer;
