fe586408011ba10ba12fc26edf2dae5b0d43006d
Kamil <kamil.malesa1@gmail.com>
2022-09-12 19:42:15 +0200

    added routing

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,19 @@
 import type { NextPage } from "next";
-
+import Link from "next/link";
 import StoreProvider from "../store/StoreProvider";
 
 import Header from "../components/Header";
 import HomeScreen from "../components/HomeScreen";
+import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
   return (
     <StoreProvider>
       <Header />
-      <HomeScreen />
+      <Link href="/">
+        <HomeScreen />
+      </Link>
+      <Footer />
     </StoreProvider>
   );
 };
