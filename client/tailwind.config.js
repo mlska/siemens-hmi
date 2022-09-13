86a1aa7df7c998f32dca9a2a927cd23307a2ccb1
Kamil Malesa <kamil.malesa1@gmail.com>
2022-09-13 22:01:58 +0200

    added animations config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,18 +7,32 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
+        opacity: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
         scale: {
           "0%": { transform: "scale(0)" },
           "100%": { transform: "scale(1)" },
         },
-        scaletransform: {
-          "0%": { transform: "scale(0) translate(-50%,-50%)" },
+        scaletranslate: {
+          "0%": { transform: "scale(0) translate(0%,0%)" },
           "100%": { transform: "scale(1) translate(-50%,-50%)" },
+        },
+        translate: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+        translate: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0%)" },
         },
       },
       animation: {
-        scale: "scale 500ms",
-        scaletransform: "scaletransform 300ms",
+        opacity: "opacity 300ms",
+        scale: "scale 300ms",
+        scaletranslate: "scaletranslate 300ms forwards",
+        translate: "translate 500ms forwards",
       },
     },
     fontFamily: {
