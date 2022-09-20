9c2dc8ef4c4b7a3c23fa950d78dc970858e350e4
Kamil <kamil.malesa1@gmail.com>
2022-09-20 21:42:57 +0200

    added alarms and warnings

--- client/store/StoreProvider.tsx
+++ client/store/StoreProvider.tsx
@@ -6,8 +6,10 @@ import React, {
   ReactNode,
 } from "react";
 import { io, Socket } from "socket.io-client";
-import { IVariables, IUser } from "../types";
+import { IVariables, IUser, IMessage } from "../types";
 import { defaultVariables, defaultUser } from "../helpers/defaults";
+import { wordToAlarms } from "../helpers/functions";
+import { alarmWord1 } from "../helpers/messages";
 
 interface iContext {
   variables: IVariables;
@@ -16,6 +18,8 @@ interface iContext {
   handleLogin: (props: IUser) => void;
   isNavExpanded: boolean;
   toggleNav: () => void;
+  alarms: Array<IMessage>;
+  warnings: Array<IMessage>;
 }
 
 export const StoreContext = createContext<iContext | undefined>(undefined);
@@ -32,12 +36,19 @@ const StoreProvider: FC<Props> = ({ children }) => {
   const [variables, setVariables] = useState<IVariables>(defaultVariables);
   const [user, setUser] = useState<IUser>(defaultUser);
   const [isNavExpanded, setIsNavExpanded] = useState<boolean>(false);
+  const [alarms, setAlarms] = useState<Array<IMessage>>([]);
+  const [warnings, setWarnings] = useState<Array<IMessage>>([]);
 
   useEffect(() => {
     socket.on("data", (data) => {
       setVariables(data);
     });
-  });
+  }, []);
+
+  useEffect(() => {
+    setAlarms(wordToAlarms(variables.wStatusWord1, alarmWord1));
+    setWarnings(wordToAlarms(variables.wControlWord1, alarmWord1));
+  }, [variables]);
 
   const setVariable = (name: string, value: number) => {
     socket.emit("control", { name, value });
@@ -60,6 +71,8 @@ const StoreProvider: FC<Props> = ({ children }) => {
         handleLogin,
         isNavExpanded,
         toggleNav,
+        alarms,
+        warnings,
       }}
     >
       {children}
