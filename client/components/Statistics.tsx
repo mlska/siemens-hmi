fac62a6b4d2ddc69abc4f719de84d4a33514263d
Kamil <kamil.malesa1@gmail.com>
2022-09-21 20:04:47 +0200

    added content board

--- client/components/Statistics.tsx
+++ client/components/Statistics.tsx
@@ -1,11 +1,22 @@
-import React, { FC } from "react";
-
+import React, { FC, useContext, useEffect } from "react";
+import { StoreContext } from "../store/StoreProvider";
+import { ScreenName } from "../types";
 interface IStatsProps {}
 
 const Statistics: FC<IStatsProps> = () => {
+  const hmi = useContext(StoreContext);
+
+  useEffect(() => {
+    hmi?.handleScreenName(ScreenName.Statistics);
+  });
+
   return (
-    <div className="flex flex-col items-center justify-center gap-4 my-16 grow">
-      Stats Component
+    <div className="flex gap-4 p-4 bg-gray-200 grow">
+      <section className="w-full p-4 mr-16 bg-white shadow-lg rounded-2xl">
+        <h1 className="text-3xl font-bold text-gray-800 font-barlow">
+          Statystyki
+        </h1>
+      </section>
     </div>
   );
 };
