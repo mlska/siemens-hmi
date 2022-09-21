7560523cf6736ce53093a2b2e97d392caa9d5699
Kamil <kamil.malesa1@gmail.com>
2022-09-21 20:03:53 +0200

    added navigation

--- client/components/Settings.tsx
+++ client/components/Settings.tsx
@@ -1,27 +1,40 @@
-import React, { FC } from "react";
-
+import React, { FC, useContext, useEffect, useState } from "react";
+import { StoreContext } from "../store/StoreProvider";
+import { ScreenName } from "../types/index";
 interface ISettingsProps {}
 
 const Settings: FC<ISettingsProps> = () => {
+  const hmi = useContext(StoreContext);
+
+  const contents = ["system", "ekran", "czujniki", "parametry"];
+
+  const [content, setContent] = useState<string>("system");
+
+  const NavigationComponent = contents.map((element) => {
+    return (
+      <button
+        className={`p-4 text-white capitalize ${
+          content === element ? "bg-blue-800" : "bg-blue-600"
+        } rounded-xl`}
+        onClick={() => setContent(element)}
+      >
+        {element}
+      </button>
+    );
+  });
+
+  useEffect(() => {
+    hmi?.handleScreenName(ScreenName.Settings);
+  });
+
   return (
     <div className="flex gap-4 p-4 bg-gray-200 grow">
       <nav className="flex flex-col w-56 gap-4 p-4 bg-white shadow-lg rounded-2xl">
-        <button
-          className="p-4 text-white bg-blue-600 rounded-xl"
-          onClick={() => console.log("clicked")}
-        >
-          System
-        </button>
-        <button
-          className="p-4 text-white bg-blue-600 rounded-xl"
-          onClick={() => console.log("clicked")}
-        >
-          Ekran
-        </button>
+        {NavigationComponent}
       </nav>
       <section className="p-4 mr-16 bg-white shadow-lg basis-10/12 rounded-2xl schrink">
         <h1 className="text-3xl font-bold text-gray-800 font-barlow">
-          Ustawienia system
+          {`Ustawienia ${content}`}
         </h1>
       </section>
     </div>
