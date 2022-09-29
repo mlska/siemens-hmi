fa49b167ad5df889f24099f4da768e501473df01
Kamil <kamil.malesa1@gmail.com>
2022-09-29 22:33:01 +0200

    code cleaning

--- client/components/Recipes.tsx
+++ client/components/Recipes.tsx
@@ -20,18 +20,41 @@ const Recipes: FC<IRecipesProps> = () => {
     hmi?.handleScreenName(ScreenName.Recipes);
   });
 
-  const optionsList = hmi?.recipes
-    .sort((a, b) => a.id - b.id)
-    .map((recipe, index) => {
-      return (
-        <option
-          value={index}
-          key={recipe.id}
-        >{`${recipe.id} - ${recipe.name}`}</option>
-      );
-    });
+  const RecipeSelectionComponent = () => {
+    const optionsList = hmi?.recipes
+      .sort((a, b) => a.id - b.id)
+      .map((recipe, index) => {
+        return (
+          <option
+            value={index}
+            key={recipe.id}
+          >{`${recipe.id} - ${recipe.name}`}</option>
+        );
+      });
 
-  const SelectedRecipe = () => {
+    return (
+      <div className="relative inline-block w-full">
+        <select
+          value={selected}
+          onChange={handleSelected}
+          className="block w-full px-4 py-2 pr-8 leading-tight bg-white border border-gray-400 rounded shadow appearance-none hover:border-gray-500 focus:outline-none focus:shadow-outline"
+        >
+          {optionsList}
+        </select>
+        <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
+          <svg
+            className="w-4 h-4 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+          </svg>
+        </div>
+      </div>
+    );
+  };
+
+  const RecipeParametersComponent = () => {
     if (hmi?.recipes[selected]) {
       const object = hmi?.recipes[selected];
       const params = Object.keys(object).map((key) => {
@@ -45,9 +68,11 @@ const Recipes: FC<IRecipesProps> = () => {
           </li>
         );
       });
-      return <ul className="flex flex-col gap-4 pt-4 mt-8">{params}</ul>;
+      return <ul className="flex flex-col gap-4 pt-4 mt-4">{params}</ul>;
     } else {
-      return <ul>Parametry niedostępne</ul>;
+      return (
+        <ul className="flex flex-col gap-4 pt-4 mt-8">Parametry niedostępne</ul>
+      );
     }
   };
 
@@ -62,37 +87,20 @@ const Recipes: FC<IRecipesProps> = () => {
             <button className="p-2 mr-4 text-blue-600 bg-gray-200 rounded-lg">
               <AiOutlineFileAdd />
             </button>
-            <button className="p-2 mr-4 bg-gray-200 rounded-lg">
+            <button className="p-2 mr-4 text-gray-800 bg-gray-200 rounded-lg">
               <BsTrash />
             </button>
-            <button className="p-2 bg-gray-200 rounded-lg">
+            <button className="p-2 text-gray-800 bg-gray-200 rounded-lg">
               <BsPencil />
             </button>
           </div>
         </div>
-        <div className="relative inline-block w-full">
-          <select
-            value={selected}
-            onChange={handleSelected}
-            className="block w-full px-4 py-2 pr-8 leading-tight bg-white border border-gray-400 rounded shadow appearance-none hover:border-gray-500 focus:outline-none focus:shadow-outline"
-          >
-            {optionsList}
-          </select>
-          <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
-            <svg
-              className="w-4 h-4 fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-            </svg>
-          </div>
-        </div>
+        <RecipeSelectionComponent />
         <div>
           <h2 className="pt-4 mt-8 text-2xl font-bold text-gray-800 border-t-2 font-barlow">
             Parametry
           </h2>
-          <SelectedRecipe />
+          <RecipeParametersComponent />
         </div>
       </section>
       <section className="flex flex-col w-full p-4 mr-16 bg-white shadow-lg rounded-2xl">
